ce528a504f4ed93dcd11d9dc2bf145bd90b11441
BrianDang03 <124744302+BrianDang03@users.noreply.github.com>
2025-04-23 21:14:54 -0600

    Edge Case For Channel Name

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from irc_interface import TalkToClient
 
+def EnsureHashtag(s):
+    if not s.startswith("#"):
+        return "#" + s
+    return s
+
 def main():
     # Prompt User
     ipAddress = input("Enter IP Address: ")
     portNumber = input("Enter Port Number: ")
     channel = input ("Enter a Channel: ")
+    channel = EnsureHashtag(channel)
 
     # Talk to Client
     TalkToClient(ipAddress, int(portNumber), channel)
+    
 main()
